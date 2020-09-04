5dd95c73d8f8e39e901867023b6f4c8fabfed6f2
Josiah <josiahbuhayan@gmail.com>
2020-09-04 08:29:43 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import time
+import speedtest
+import datetime
+
+
+while True:
+
+    f = open("internet_speed_monitor.csv", "a")
+    st = speedtest.Speedtest()
+    dl_speed = st.download() / 1000000
+    up_speed = st.upload() / 1000000
+    latency = st.results.ping
+    timestamp = datetime.datetime.now()
+
+    print("Down: ",dl_speed)
+    print("Up: ",up_speed)
+    print(latency)
+
+    f.write(str(timestamp))
+    f.write(",")
+    f.write(str(dl_speed))
+    f.write(",")
+    f.write(str(up_speed))
+    f.write(",")
+    f.write(str(latency))
+    f.write(" ms")
+    f.write("\n")
+    f.close()
+
+    time.sleep(600)

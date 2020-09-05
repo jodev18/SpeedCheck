098e0094b63752646204fee498ecd619b0dfb7d6
Josiah <josiahbuhayan@gmail.com>
2020-09-05 19:47:34 +0800

    Data update

--- main.py
+++ main.py
@@ -6,25 +6,40 @@ import datetime
 while True:
 
     f = open("internet_speed_monitor.csv", "a")
-    st = speedtest.Speedtest()
-    dl_speed = st.download() / 1000000
-    up_speed = st.upload() / 1000000
-    latency = st.results.ping
-    timestamp = datetime.datetime.now()
+    try:
+        st = speedtest.Speedtest()
+        dl_speed = st.download() / 1000000
+        up_speed = st.upload() / 1000000
+        latency = st.results.ping
+        timestamp = datetime.datetime.now()
 
-    print("Down: ",dl_speed)
-    print("Up: ",up_speed)
-    print(latency)
+        print("Down: ", dl_speed)
+        print("Up: ", up_speed)
+        print(latency)
 
-    f.write(str(timestamp))
-    f.write(",")
-    f.write(str(dl_speed))
-    f.write(",")
-    f.write(str(up_speed))
-    f.write(",")
-    f.write(str(latency))
-    f.write(" ms")
-    f.write("\n")
-    f.close()
+        f.write(str(timestamp))
+        f.write(",")
+        f.write(str(dl_speed))
+        f.write(",")
+        f.write(str(up_speed))
+        f.write(",")
+        f.write(str(latency))
+        f.write(" ms")
+        f.write("\n")
+        f.close()
 
-    time.sleep(600)
+        time.sleep(600)
+    except:
+        timestamp = datetime.datetime.now()
+        f.write(str(timestamp))
+        f.write(",")
+        f.write("0")
+        f.write(",")
+        f.write("0")
+        f.write(",")
+        f.write("0")
+        f.write("--")
+        f.write("\n")
+        f.close()
+        print("DNS Down")
+        time.sleep(10)
